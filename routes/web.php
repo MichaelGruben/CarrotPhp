b7166dae5eaeeaed4e62fb022c64d3a87ccc540c
Michael Gruben <michael.gruben@gmx.net>
2021-03-14 17:14:35 +0100

    add custom route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', function()
+{
+    return 'Users!';
+});
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
